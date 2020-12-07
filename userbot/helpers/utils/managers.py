eb5f85f9693ad9c6faed69dbcfeff640ce5916b6
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-12-07 16:43:13 +0000

    pylint: auto fixes

--- userbot/helpers/utils/managers.py
+++ userbot/helpers/utils/managers.py
@@ -1,16 +1,22 @@
 import requests
 
+
 def paste_text(text):
     asciich = ["*", "`", "_"]
     for i in asciich:
         text = re.sub(rf"\{i}", "", text)
     try:
-            key = (requests.post("https://nekobin.com/api/documents", json={"content": text}).json().get("result").get("key"))
-            link = f"https://nekobin.com/{key}"
+        key = (
+            requests.post("https://nekobin.com/api/documents", json={"content": text})
+            .json()
+            .get("result")
+            .get("key")
+        )
+        link = f"https://nekobin.com/{key}"
     except:
-            text = re.sub(r"•", ">>", text)
-            kresult = requests.post(
-                "https://del.dog/documents", data=text.encode("UTF-8")
-            ).json()
-            link = f"https://del.dog/{kresult['key']}"
-    return link 
+        text = re.sub(r"•", ">>", text)
+        kresult = requests.post(
+            "https://del.dog/documents", data=text.encode("UTF-8")
+        ).json()
+        link = f"https://del.dog/{kresult['key']}"
+    return link
