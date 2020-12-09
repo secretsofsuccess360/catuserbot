115611c9458b1e11c2b377f1f9656fe4a205d784
secretsofsuccess360 <69966275+secretsofsuccess360@users.noreply.github.com>
2020-12-09 10:39:24 +0530

    Add files via upload

---
+++ userbot/plugins/gm_gn.py
@@ -0,0 +1,159 @@
+import asyncio
+from . import CMD_HELP
+from ..utils import admin_cmd,sudo_cmd
+   
+   
+@bot.on(admin_cmd(outgoing=True, pattern="gm1$"))
+@bot.on(sudo_cmd(pattern="gm1$", allow_sudo=True))   
+async def catg1m(gm1):
+    await gm1.edit("╔═══ ✨🧚🏻✿💫🧚🏻 ══╗\n"
+"ㅤㅤㅤG๏๏๔ ๓๏гภเภG\n"
+"╚═══ 💫🧚🏻✿✨🧚🏻 ══╝\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn1$"))
+@bot.on(sudo_cmd(pattern="gn1$", allow_sudo=True)) 
+async def catg1n(gn1):
+    await gn1.edit("╔═══ ✨🧚🏻✿💫🧚🏻 ══╗\n"
+"ㅤㅤㅤG๏๏๔ ภเGђt\n"
+"╚═══ 💫🧚🏻✿✨🧚🏻 ══╝\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm2$"))
+@bot.on(sudo_cmd(pattern="gm2$", allow_sudo=True)) 
+async def catg2m(gm2):
+    await gm2.edit("══❥━━═❥═━━❥════❥\n"
+"G♥️o♥️o♥️d♥️\n"
+"M♥️o♥️r♥️n♥️i♥️n♥️g♥️\n"
+"══❥━━═❥═━━❥════❥\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn2$"))
+@bot.on(sudo_cmd(pattern="gn2$", allow_sudo=True)) 
+async def catg2n(gn2):
+    await gn2.edit("══❥━━═❥═━━❥════❥\n"
+"G♥️o♥️o♥️d♥️\n"
+"N♥i♥g♥h♥t♥\n"
+"══❥━━═❥═━━❥════❥\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm3$"))
+@bot.on(sudo_cmd(pattern="gm3$", allow_sudo=True)) 
+async def catg3m(gm3):
+    await gm3.edit("▭▬▭▬▭▬▭▬▭▬\n"
+"◄█𝙂𝙤𝙤𝙙 𝙈𝙤𝙧𝙣𝙞𝙣𝙜█►\n"
+"▭▬▭▬▭▬▭▬▭▬\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn3$"))
+@bot.on(sudo_cmd(pattern="gn3$", allow_sudo=True)) 
+async def catg3n(gn3):
+    await gn3.edit("▭▬▭▬▭▬▭▬▭▬\n"
+"◄█𝙂𝙤𝙤𝙙 𝙉𝙞𝙜𝙝𝙩█►\n"
+"▭▬▭▬▭▬▭▬▭▬\n")  
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm4$"))
+@bot.on(sudo_cmd(pattern="gm4$", allow_sudo=True)) 
+async def catg4m(gm4):
+    await gm4.edit("█▀▀▀▀▀▀▀▀▀▀▀█\n"
+"❣️❥V3rY❥900D❥M0rn!n9❣️\n"
+"█▄▄▄▄▄▄▄▄▄▄▄█\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn4$"))
+@bot.on(sudo_cmd(pattern="gn4$", allow_sudo=True)) 
+async def catg4n(gn4):
+    await gn4.edit("█▀▀▀▀▀▀▀▀▀▀▀█\n"
+"❣️900D❥N!9h7❥5!33pw3!❣️\n"
+"█▄▄▄▄▄▄▄▄▄▄▄█\n") 
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm5$"))
+@bot.on(sudo_cmd(pattern="gm5$", allow_sudo=True)) 
+async def catg5m(gm5):
+    await gm5.edit("┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n"
+"G🌷o🍃o🌷D\n"
+"M🍃o🌷r🍃n🌷i🍃n🌷G\n"
+"┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n") 
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn5$"))
+@bot.on(sudo_cmd(pattern="gn5$", allow_sudo=True)) 
+async def catg5n(gn5):
+    await gn5.edit("┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n"
+"G🌷o🍃o🌷D\n"
+"N🍃i🌷G🍃h🌷T\n"
+"┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n")   
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm6$"))
+@bot.on(sudo_cmd(pattern="gm6$", allow_sudo=True)) 
+async def catg6m(gm6):
+    await gm6.edit("。.・゜✭・.・✫・・゜✭・..・✫\n"
+"⌯ 🕷•°°•ğ¤¤ď ɱ¤ȑñȋñğ•°°•🕷⌯ .\n"
+"。.・゜✭・.・✫・・゜✭・..・✫\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn6$"))
+@bot.on(sudo_cmd(pattern="gn6$", allow_sudo=True)) 
+async def catg6n(gn6):
+    await gn6.edit("。.・゜✭・.・✫・・゜✭・..・✫\n"
+"⌯ 🕷•°°• ğ¤¤ď ñȋğȟț •°°•🕷⌯ .\n"
+"。.・゜✭・.・✫・・゜✭・..・✫\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm7$"))
+@bot.on(sudo_cmd(pattern="gm7$", allow_sudo=True)) 
+async def catg7m(gm7):
+    await gm7.edit("┌──❀*̥˚──◌──❀*̥˚─┐\n"
+"   𖤐 𝘎𝘔....𝘏𝘢𝘷𝘦 𝘢 𝘨𝘙8 𝘥𝘈𝘺 𖤐\n"
+"└◌─❀*̥˚ ──◌───❀*̥˚┘\n")    
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn7$"))
+@bot.on(sudo_cmd(pattern="gn7$", allow_sudo=True)) 
+async def catg7n(gn7):
+    await gn7.edit("┌──❀*̥˚──◌──❀*̥˚─┐\n"
+"𝒢𝒩...𝒮𝐿𝐸𝐸𝒫 𝒲𝐸𝐿𝐿....\n"
+"└◌─❀*̥˚ ──◌───❀*̥˚┘\n")   
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm8$"))
+@bot.on(sudo_cmd(pattern="gm8$", allow_sudo=True)) 
+async def catg8m(gm8):
+    await gm8.edit("•『 𝒢𝑜𝑜𝒹 𝑀𝑜𝓇𝓃𝒾𝓃𝑔♡ 』•\n")
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn8$"))
+@bot.on(sudo_cmd(pattern="gn8$", allow_sudo=True)) 
+async def catg8n(gn8):
+    await gn8.edit("•『 𝒢𝑜𝑜𝒹 𝒩𝒾𝑔𝒽𝓉...♡ 』•\n")     
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gm9$"))
+@bot.on(sudo_cmd(pattern="gm9$", allow_sudo=True)) 
+async def catg9m(gm9):
+    await gm9.edit("(\_/)\n"
+"(●_●)\n"
+" />🌹 🅶🅼\n"
+"🅷🅰️🆅🅴_🅰️_🅶🆁8_🅳🅰️🆈\n")  
+    
+
+@bot.on(admin_cmd(outgoing=True, pattern="gn9$"))
+@bot.on(sudo_cmd(pattern="gn9$", allow_sudo=True)) 
+async def catg9n(gn9):
+    await gn9.edit("(\_/)\n"
+"(●_●)\n"
+" />🌹 🅶🅽\n"
+"🅶🅽_🆂🅻🅴🅴P_🆆🅴🅻🅻_🆃🅲\n")    
+    
+
+CMD_HELP.update(
+    {
+        "gm-gn": "__**PLUGIN NAME :** gm-gn__\
+\n\n📌** CMD ➥** `.gm1`  `.gn1`  `.gm2`  `.gn2`  `.gm3`  `.gn3`  `.gm4`  `.gn4`  `.gm5`  `.gn5`  `.gm6`  `.gn6`  `.gm7`  `.gn7`  `.gm8`   `.gn8`  `.gm9` \
+\n\n**USAGE   ➥  **gm and gn wishes.\
+"
+    }
+)
