8df9002002468d51176335f35b9a71f8c51760f2
secretsofsuccess360 <69966275+secretsofsuccess360@users.noreply.github.com>
2020-12-09 12:03:38 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,8 +8,8 @@ from . import ALIVE_NAME, CMD_HELP, StartTime, catdef, catversion, mention, repl
 
 DEFAULTUSER = ALIVE_NAME or "cat"
 CAT_IMG = Config.ALIVE_PIC
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✮ MY BOT IS RUNNING SUCCESFULLY ✮"
 EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ✥ "
+CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or f"{EMOJI} MY BOT IS RUNNING SUCCESFULLY {EMOJI}"
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
@@ -22,12 +22,12 @@ async def amireallyalive(alive):
     _, check_sgnirts = check_data_base_heal_th()
     if CAT_IMG:
         cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-        cat_caption += f"**{EMOJI} Database :** `{check_sgnirts}`\n"
-        cat_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
-        cat_caption += f"**{EMOJI} Userbot Version :** `{catversion}`\n"
-        cat_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-        cat_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
-        cat_caption += f"**{EMOJI} Master:** {mention}\n"
+        cat_caption += f"**➥ Database :** `{check_sgnirts}`\n"
+        cat_caption += f"**➥ Telethon version :** `{version.__version__}\n`"
+        cat_caption += f"**➥ Userbot Version :** `{catversion}`\n"
+        cat_caption += f"**➥ Python Version :** `{python_version()}\n`"
+        cat_caption += f"**➥ Uptime :** `{uptime}\n`"
+        cat_caption += f"**➥ Master:** {mention}\n"
         await alive.client.send_file(
             alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id
         )
@@ -36,12 +36,12 @@ async def amireallyalive(alive):
         await edit_or_reply(
             alive,
             f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-            f"**{EMOJI} Database :** `{check_sgnirts}`\n"
-            f"**{EMOJI} Telethon Version :** `{version.__version__}\n`"
-            f"**{EMOJI} Userbot Version :** `{catversion}`\n"
-            f"**{EMOJI} Python Version :** `{python_version()}\n`"
-            f"**{EMOJI} Uptime :** `{uptime}\n`"
-            f"**{EMOJI} Master:** {mention}\n",
+            f"**➥ Database :** `{check_sgnirts}`\n"
+            f"**➥ Telethon Version :** `{version.__version__}\n`"
+            f"**➥ Userbot Version :** `{catversion}`\n"
+            f"**➥ Python Version :** `{python_version()}\n`"
+            f"**➥ Uptime :** `{uptime}\n`"
+            f"**➥ Master:** {mention}\n",
         )
 
 
@@ -52,11 +52,11 @@ async def amireallyalive(alive):
         return
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     reply_to_id = await reply_id(alive)
-    cat_caption = f"** 𝑫𝑹𝑻 '𝑺 Userbot is Up and Running**\n\n"
-    cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**  -Userbot Version :** `{catversion}`\n"
-    cat_caption += f"**  -Python Version :** `{python_version()}\n`"
-    cat_caption += f"**  -Master:** {mention}\n"
+    cat_caption = f"** {EMOJI}𝑫𝑹𝑻 '𝑺 Userbot is Up and Running{EMOJI}**\n\n"
+    cat_caption += f"**  ➥ Telethon version :** `{version.__version__}\n`"
+    cat_caption += f"**  ➥ Userbot Version :** `{catversion}`\n"
+    cat_caption += f"**  ➥ Python Version :** `{python_version()}\n`"
+    cat_caption += f"**  ➥ Master:** {mention}\n"
     results = await bot.inline_query(tgbotusername, cat_caption)  # pylint:disable=E0602
     await results[0].click(alive.chat_id, reply_to=reply_to_id, hide_via=True)
     await alive.delete()
