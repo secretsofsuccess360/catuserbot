41b3229c7651262ad7fe40e9694a50bd2d856950
secretsofsuccess360 <69966275+secretsofsuccess360@users.noreply.github.com>
2020-12-20 15:25:54 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -109,13 +109,15 @@ async def on_afk(event):
         msg = None
         if link and reason:
             message_to_reply = (
-                f"**I am AFK .\n\nAFK Since {endtime}\nReason : **{reason}"
+                f"**I am AFK**\n\n**AFK Since :** `{endtime}`\n**Reason : **{reason}"
             )
         elif reason:
-            message_to_reply = f"`I am AFK .\n\nAFK Since {endtime}\nReason : {reason}`"
+            message_to_reply = (
+                f"**I am AFK\n\nAFK Since :** `{endtime}`\n**Reason : **`{reason}`"
+            )
         else:
             message_to_reply = (
-                f"`I am AFK .\n\nAFK Since {endtime}\nReason : Not Mentioned ( ಠ ʖ̯ ಠ)`"
+                f"`I am AFK\n\nAFK Since :{endtime}\nReason : Not Mentioned ( ಠ ʖ̯ ಠ)`"
             )
         if event.chat_id not in Config.UB_BLACK_LIST_CHAT:
             msg = await event.reply(message_to_reply)
@@ -186,13 +188,12 @@ async def _(event):
 
 CMD_HELP.update(
     {
-        "afk": "**Plugin : **`afk`\
-        \n\n  •  **Syntax : **`.afk [Optional Reason]`\
-        \n  •  **Function : **__Sets you as afk.\nReplies to anyone who tags/PM's \
-        \you telling them that you are AFK(reason).\n\nSwitches off AFK when you type back anything, anywhere.\
-        \afk means away from keyboard/keypad.__\
-        \n\n  •  **Note :** If you want AFK with hyperlink use [ ; ] after reason, then paste the media link.\
-        \n  •  **Example :** `.afk busy now ;<Media_link>`\
+        "afk": "__**PLUGIN NAME :** Afk__\
+\n\n📌** CMD ➥** `.afk` [Optional Reason]\
+\n**USAGE   ➥  **Sets you as afk.\nReplies to anyone who tags/PM's \
+you telling them that you are AFK(reason)\n\n__Switches off AFK when you type back anything, anywhere.__\
+\n\n**Note :** If you want AFK with hyperlink use [ ; ] after reason, then paste the media link.\
+\n**Example :** `.afk busy now ;<Media_link>`\
 "
     }
 )
